fbb339df7d55f79af77d1176a2eae472fa2cc488
Hashir <hashir.logic@gmail.com>
2025-08-29 14:27:13 +0500

    Initial commit for .NET

---
+++ Task-06/CalculatorLibrary/CalculatorLibrary/math2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class math2
+    {
+        public double square(int a)
+        {
+            return a * a;
+        }
+        public double rectangle(int a, int b)
+        {
+            return a * b;
+        }
+        public double traingle(int a, int b)
+        {
+            return 1 / 2 * (a * b);
+        }
+        public double circle(int a)
+        {
+            return 2 * 3.14 * a * a;
+        }
+    }
+}
