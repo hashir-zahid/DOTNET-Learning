4a26752f7052c8e194157065db109585865ca4ef
Hashir <hashir.logic@gmail.com>
2025-08-29 14:24:13 +0500

    Initial commit for .NET

---
+++ Task-06/CalculatorLibrary/CalculatorLibrary/math.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class math
+    {
+        public double add(int a, int b)
+        {
+            return a + b;
+        }
+        public double sub(int a, int b)
+        {
+            return a - b;
+        }
+        public double mul(int a, int b)
+        {
+            return a * b;
+        }
+        public double div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
