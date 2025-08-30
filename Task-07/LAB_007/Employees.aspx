e574f2977dec2fbbade8fdd87817be51a9a3d9d3
Hashir <hashir.logic@gmail.com>
2025-08-30 20:09:34 +0500

    Initial commit for .NET

---
+++ Task-07/LAB_007/Employees.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="LAB_007.Employees" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" 
+Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %> 
+<!DOCTYPE html> 
+<html> 
+<head runat="server"> 
+<title>Employee Report</title> 
+</head> 
+<body> 
+<form id="form1" runat="server"> 
+<asp:ScriptManager ID="ScriptManager1" 
+runat="server"></asp:ScriptManager> 
+<div> 
+<asp:Button ID="btnViewReport" runat="server" Text="View Report" 
+OnClick="btnViewReport_Click" /> 
+<asp:Button ID="btnPrint" runat="server" Text="Print Report" 
+OnClientClick="printReport(); return false;" /> 
+</div> 
+<rsweb:ReportViewer ID="ReportViewer1" runat="server" Width="100%" 
+Height="600px" ProcessingMode="Local"> 
+        </rsweb:ReportViewer> 
+    </form> 
+ 
+    <script> 
+        function printReport() {
+            var reportFrame = document.getElementById('<%= 
+ReportViewer1.ClientID %>');
+            if (reportFrame) {
+                var printWindow =
+                    reportFrame.getElementsByTagName("iframe")[0].contentWindow;
+                printWindow.focus();
+                printWindow.print();
+            } else {
+                alert("Report Viewer not found!");
+            }
+        }
+    </script> 
+</body> 
+</html>
+
---
+++ Task-07/LAB_007/Employees.aspx.cs
@@ -0,0 +1,50 @@
+﻿using LAB_007.EmployeesDataSetTableAdapters;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB_007
+{
+    public partial class Employees : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                ReportViewer1.Visible = true;
+            }
+        }
+
+        protected void btnViewReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EmployeesTableAdapter tableAdapter = new EmployeesTableAdapter();
+                DataTable dataTable = tableAdapter.GetData();
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    ReportViewer1.LocalReport.DataSources.Clear();
+                    ReportDataSource rds = new ReportDataSource("DataSet1", dataTable);
+                    ReportViewer1.LocalReport.DataSources.Add(rds);
+                    ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Employees.rdlc");
+
+                    ReportViewer1.Visible = true;
+                }
+                else
+                {
+                    Response.Write("else");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("catch");
+            }
+        }
+    }
+}
