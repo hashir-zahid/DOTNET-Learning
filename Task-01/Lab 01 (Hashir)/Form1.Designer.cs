1af955808c8bee8a315fbe81edd8a7be5e03989b
Hashir <hashir.logic@gmail.com>
2025-09-05 14:38:23 +0500

    Initial commit for .NET

---
+++ Task-01/Lab 01 (Hashir)/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Lab_01__Hashir_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.checkBox3.Location = new System.Drawing.Point(623, 271);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 28);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "Italic";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.checkBox2.Location = new System.Drawing.Point(623, 207);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(115, 28);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Underline";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.checkBox1.Location = new System.Drawing.Point(626, 141);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 28);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Bold";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button2.Location = new System.Drawing.Point(311, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 52);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Calculator";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Location = new System.Drawing.Point(340, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 52);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 36);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Hello World";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Log-In";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Task-01/Lab 01 (Hashir)/Form2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_01__Hashir_
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightGreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += ".";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 0;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 3;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 4;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 5;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 6;
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 7;
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 8;
+        }
+
+        private void button11_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 9;
+        }
+
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string expression = richTextBox1.Text;
+                DataTable table = new DataTable();
+                var result = table.Compute(expression, "");
+
+                richTextBox1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Error";
+            }
+        }
+
+        private void button16_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "+";
+        }
+
+        private void button15_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "-";
+        }
+
+        private void button14_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "*";
+        }
+
+        private void button13_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "/";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, 20);
+        }
+    }
+}
