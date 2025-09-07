34189b0cf18bbcaa55e98fa440307dc8ffc340d4
Hashir <hashir.logic@gmail.com>
2025-09-07 18:00:03 +0500

    Initial commit for .NET

---
+++ Task-02/lab02/lab02/DBconn.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab02
+{
+    class DBconn
+    {
+        private string connectionString = "Data Source = DESKTOP-SDI7IH1\\SQLEXPRESS;Initial Catalog=Hashir;Integrated Security=True";
+        public bool Signup(string username, string password, string email, string position, string department)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Employees(username, password, email, position, department)  " +
+                        "VALUES(@username, @password, @email, @position, @department)";
+                    SqlCommand command = new SqlCommand(query, conn);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@position", position);
+                    command.Parameters.AddWithValue("@department", department);
+
+                    conn.Open();
+
+                    int result = command.ExecuteNonQuery();
+
+                    conn.Close();
+
+                    return result > 0;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+                return false;
+            }
+
+        }
+        public bool login(string username, string password)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "select * from Employees where username=@username AND password=@password";
+                    SqlCommand command = new SqlCommand(query, conn);
+
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    conn.Open();
+
+                    int result = Convert.ToInt32(command.ExecuteScalar());
+
+                    conn.Close();
+
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Task-02/lab02/lab02/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab02
+{
+    public partial class Form2: Form
+    {
+        DBconn db = new DBconn();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            bool success = db.login(username, password);
+            if (success)
+            {
+                MessageBox.Show("User Exist");
+            }
+            else
+            {
+                MessageBox.Show("User Does Not Exist");
+            }
+        }
+    }
+}
